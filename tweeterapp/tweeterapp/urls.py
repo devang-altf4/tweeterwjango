fb9f3fed7bc0b9d71fc763aa4f08252df2e9c0e0
Devang gupta <maybedevang29@gmail.com>
2026-01-17 16:58:59 +0530

    g

--- tweeterapp/tweeterapp/urls.py
+++ tweeterapp/tweeterapp/urls.py
@@ -19,10 +19,13 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import include
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tweet/', include('tweet.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    
+    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
